e102ec6def46339ca9184eeca6274eb65cdee2d0
Leonardo Pontes <firstname.leopontes.1989@gmail.com>
2023-08-18 17:44:06 -0300

    Requisito 3 e 4, Criação da função helper renderWithRouter, e implementação de funcionalidades dos inputs e botão.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,22 +1,51 @@
+import { useState } from 'react';
 import style from './Login.module.css';
 
 function Login() {
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputPass, setInputPass] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const validateEmail = /\S+@\S+\.\S+/;
+
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if ((validateEmail.test(inputEmail)) && (inputPass.length > 6)) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+    setInputEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if ((validateEmail.test(inputEmail)) && (inputPass.length + 1 >= 6)) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+    setInputPass(e.target.value);
+  };
+
   return (
     <div className={ style.container }>
+      <h1 className={ style.heading }>Login</h1>
       <input
         className={ style.inputLogin }
         type="email"
         data-testid="email-input"
+        onChange={ handleChangeEmail }
       />
       <input
         className={ style.inputLogin }
         type="password"
         data-testid="password-input"
+        onChange={ handleChangePassword }
       />
       <button
         className={ style.btnLogin }
         type="submit"
         data-testid="login-submit-btn"
+        disabled={ isDisabled }
       >
         Enter
       </button>
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { screen } from '@testing-library/react';
+import { renderWithRouter } from '../helpers/renderWithRouter';
+import Login from '../pages/Login/Login';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testes referentes à página Login.tsx', () => {
+  test('Verifica se o Heading e os Inputs existem na página', () => {
+    renderWithRouter(<Login />);
+    const heading = screen.getByRole('heading', { name: /login/i });
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const buttonEnter = screen.getByRole('button', { name: /enter/i });
+
+    expect(heading).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(buttonEnter).toBeInTheDocument();
+  });
 });
