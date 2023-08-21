c37cbf68bd91f8447abae801e7137860e07f1555
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2023-08-21 15:35:30 -0300

    fix: mock fetch recipesDetails test fix

--- src/tests/RecipesDetails.test.tsx
+++ src/tests/RecipesDetails.test.tsx
@@ -1,8 +1,17 @@
 import { screen } from '@testing-library/react';
+import { vi } from 'vitest';
 import RecipeDetails from '../pages/RecipeDetails/RecipeDetails';
 import { renderWithRouter } from './helpers/renderWith';
+import mockSearchMeal from './mocks/mockSearchMeal';
+import { mockRecipeCategories } from './mocks/mockRecipesList';
 
 describe('Testes referentes ao componente RecipesDetails', () => {
+  global.fetch = vi.fn().mockResolvedValueOnce({
+    json: async () => mockSearchMeal,
+  })
+    .mockResolvedValueOnce({
+      json: async () => mockRecipeCategories,
+    });
   renderWithRouter(<RecipeDetails scope="meals" />);
   test('Verifica se o componente contém um heading h2', () => {
     const heading = screen.getByRole('heading', { level: 2 });
