f786ca976c4712960bcbf1a91a42740d157b8b21
Raiane Yanagita - Turma 32 - Tribo A <raianemayumeyanagita@gmail.com>
2023-08-22 17:14:19 -0300

    feat(profile): req 58 e 59

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -4,13 +4,15 @@ import { GlobalContext } from '../../contexts/global.context';
 
 function Profile() {
   const { state } = useContext(GlobalContext);
+  const getEmail = state.user.email
+  || JSON.parse(localStorage.getItem('user') ?? '').email;
   return (
     <>
       <Header
         pageTitle="Profile"
         searchIcon={ false }
       />
-      <div data-testid="profile-email">{state.user.email}</div>
+      <div data-testid="profile-email">{getEmail}</div>
       <button type="button" data-testid="profile-done-btn">Done Recipes</button>
       <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
       <button type="button" data-testid="profile-logout-btn">Logout</button>
