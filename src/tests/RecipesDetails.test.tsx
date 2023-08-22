d7ec20176ada46ae842f29ae83425e9abb00ab52
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2023-08-22 16:47:58 -0300

    feat: share and fav buttons plus tests coverage

--- src/tests/RecipesDetails.test.tsx
+++ src/tests/RecipesDetails.test.tsx
@@ -1,7 +1,6 @@
 import { screen, waitFor } from '@testing-library/react';
 import { vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
-import RecipeDetails from '../pages/RecipeDetails/RecipeDetails';
 import { renderWithRouter } from '../helpers/renderWithRouter';
 import mockSearchMeal from './mocks/mockSearchMeal';
 import { mockRecipeCategories } from './mocks/mockRecipesList';
