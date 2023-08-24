f5cac8b93a35e9e41b4b0f619fcb2aac3139023b
Leonardo Pontes <firstname.leopontes.1989@gmail.com>
2023-08-24 01:31:53 -0300

    Implementação do botão home em cima do título do app

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,25 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ProfileButton from './ProfileButton';
 import SearchButton from './SearchButton';
 import style from './Header.module.css';
 import trayImg from '../../images/tray.svg';
-import mealImg from '../../images/mealIcon2.svg';
 import SearchInput from '../SearchBar';
 
 type HeaderProps = {
-  pageTitle: string;
   searchIcon?: boolean;
 };
 
-export default function Header({ pageTitle, searchIcon = true }: HeaderProps) {
+export default function Header({ searchIcon = true }: HeaderProps) {
   const [searchOn, setSearchOn] = useState(false);
+  const navigate = useNavigate();
   return (
     <header className={ style.headerContainer }>
       <div className={ style.TopHeaderContainer }>
-        <div className={ style.headerTitle }>
+        <button
+          className={ style.headerTitle }
+          onClick={ () => navigate('/meals') }
+        >
           <img src={ trayImg } alt="meal icon" />
           <h1
             data-testid="page-title"
@@ -26,7 +29,7 @@ export default function Header({ pageTitle, searchIcon = true }: HeaderProps) {
             {' '}
             <span className={ style.titleSpan }>receitas</span>
           </h1>
-        </div>
+        </button>
         <div className={ style.icons }>
           { searchIcon && <SearchButton barVisible={ () => setSearchOn(!searchOn) } /> }
           <ProfileButton />
