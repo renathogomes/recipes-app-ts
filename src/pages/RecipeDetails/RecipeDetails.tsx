433f02f35b7c3c0413db9141ebfc222c2380061d
Leonardo Pontes <firstname.leopontes.1989@gmail.com>
2023-08-29 13:24:23 -0300

    Ajustes de estilização página in-progress

--- src/pages/RecipeDetails/RecipeDetails.tsx
+++ src/pages/RecipeDetails/RecipeDetails.tsx
@@ -121,7 +121,7 @@ function RecipeDetails({ scope }: RecipesProps) {
           className={ style.iconShare }
         />
       </button>
-      { isShared && <p>Link copied!</p> }
+      { isShared && <span className={ style.spanShare }>Link copied!</span> }
       { isFavorite
         ? (
           <button
@@ -192,7 +192,7 @@ function RecipeDetails({ scope }: RecipesProps) {
       >
         { recipe?.strInstructions }
       </p>
-      <h2 className={ style.heading }>Video</h2>
+      { scope !== 'drinks' && <h2 className={ style.heading }>Video</h2> }
       { recipe?.strMeal && <iframe title="recipe video" data-testid="video" width="340" height="315" src={ `https://www.youtube.com/embed/${recipe?.strYoutube.split('=')[1]}` } /> }
       <Recommended
         type={ scope }
